0603b136d4e5adc018357bef73ca5bad7ba6cdae
Pablo Campos Huawei <pablo.c1791@gmail.com>
2022-12-05 20:22:17 +0100

    creado el itemCount - actividad 1

--- PreEntrega1-CamposPablo/src/components/ItemListContainer/ItemListContainer.js
+++ PreEntrega1-CamposPablo/src/components/ItemListContainer/ItemListContainer.js
@@ -1,7 +1,12 @@
 import React from "react";
+import ItemCount from "../ItemCount/ItemCount";
 
 const ItemListContainer = ({ saludo }) => {
-    return <p>{saludo}</p>;
+    return <div>
+        {saludo}
+        {/* a MODO DE PRUEBA COLOCO EL ITEM COUNT, LUEGO QUITARLO */}
+        <ItemCount stockInicial={5} />
+    </div>;
 }
 
 export default ItemListContainer;
